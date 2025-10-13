fdb67a9570b47f18c0071f699e50f0cd5e94befa
akashpradhan-cyber <akash.pradhan@esconinfrarealtor.com>
2025-10-13 13:47:07 +0530

    Delete Restaurent_Application-main (4) directory

--- Restaurent_Application-main (4)/src/app/restaurent-dash/restaurent.model.ts
+++
@@ -1,8 +0,0 @@
-export class RestaurentData {
-    id: number = 0;
-    name: string = '';
-    address: string = '';
-    email: string = '';
-    services: string = '';
-    mobile: number = 0;
-}
